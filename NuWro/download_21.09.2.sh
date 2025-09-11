14aff717b67fcb52b1d611fe4e0f0b2fe4835c88
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2025-09-11 15:34:15 +0000

    Adding NuWro installation guide in Alma9

---
+++ NuWro/download_21.09.2.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/NuWro/nuwro.git nuwro-src -b nuwro_21.09.2
---
+++ NuWro/setup.sh
@@ -0,0 +1,7 @@
+pythia6
+## rwdwxv6 pythia6@6.4.28%gcc@12.2.0 arch=linux-almalinux9-x86_64_v2
+spack load /rwdwxv6
+export PYTHIA6_LIB_DIR=$(spack location -i /rwdwxv6)/lib
+export PYTHIA6=${PYTHIA6_LIB_DIR}
+# For nuwro build
+export LIBRARY_PATH=${PYTHIA6}:${LIBRARY_PATH}
